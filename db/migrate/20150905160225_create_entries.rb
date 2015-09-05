cda177b855d1e0550fcef3ef710486efb9f6ca52
Dave Woodall <dave.woodall@livingsocial.com>
2015-09-05 10:22:17 -0600

    initial commit

---
+++ db/migrate/20150905160225_create_entries.rb
@@ -0,0 +1,12 @@
+class CreateEntries < ActiveRecord::Migration
+  def change
+    create_table :entries do |t|
+      t.string :title
+      t.text :body
+      t.integer :user_id
+      t.integer :tag_id
+
+      t.timestamps null: false
+    end
+  end
+end
