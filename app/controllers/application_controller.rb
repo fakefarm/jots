57a099d225fd5a4e56c08d339afd8bd6dd228ff6
Dave Woodall <dave.woodall@livingsocial.com>
2015-09-08 19:27:47 -0600

    Devise gets a different layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,19 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :create_entry
+  layout :layout_by_resource
 
   def create_entry
     @entry = Entry.new
+  end
+
+  protected
+
+  def layout_by_resource
+    if devise_controller?
+      "welcome"
+    else
+      "application"
+    end
   end
 end
