10ba55df6ad60c171878b81ffaf3ca38c2b31748
Dave Woodall <dave.woodall@livingsocial.com>
2015-09-08 19:11:43 -0600

    Entry on every screen via application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,9 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  before_action :create_entry
+
+  def create_entry
+    @entry = Entry.new
+  end
 end
