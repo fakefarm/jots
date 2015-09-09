3c6d22078cb357c90ffd6435bd95fc56bff71b3e
Dave Woodall <dave.woodall@livingsocial.com>
2015-09-09 13:47:17 -0600

    Add archives in all views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,10 +2,16 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
   before_action :create_entry
+  before_action :list_archives
   layout :layout_by_resource
 
   def create_entry
     @entry = Entry.new
+  end
+
+  def list_archives
+    @archives = Entry.where(user_id: current_user.id).
+                  inject([]) { |memo, entry| memo.push entry.created_at.to_date; memo }.uniq
   end
 
   protected
