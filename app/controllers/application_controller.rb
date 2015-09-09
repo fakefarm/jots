142d546b631d13c8548b599e0033a7a44cd8d5ad
Dave Woodall <dave.woodall@livingsocial.com>
2015-09-09 14:02:58 -0600

    Check for user before archives

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,17 +1,19 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :authenticate_user!
+  layout :layout_by_resource
   before_action :create_entry
   before_action :list_archives
-  layout :layout_by_resource
 
   def create_entry
     @entry = Entry.new
   end
 
   def list_archives
-    @archives = Entry.where(user_id: current_user.id).
-                  inject([]) { |memo, entry| memo.push entry.created_at.to_date; memo }.uniq
+    if current_user
+      @archives = Entry.where(user_id: current_user.id).
+                        inject([]) { |memo, entry| memo.push entry.created_at.to_date; memo }.uniq
+    end
   end
 
   protected
