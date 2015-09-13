b3a3e6a0c29a2d194808b3da1c8dce6b5e8ead11
Dave Woodall <dave.woodall@livingsocial.com>
2015-09-13 12:12:50 -0600

    Entry methods to manage archives

--- app/models/entry.rb
+++ app/models/entry.rb
@@ -14,8 +14,8 @@ class Entry < ActiveRecord::Base
           where(:created_at => range).order('id DESC')
   end
 
-  def self.archives_for_user(id) # _dw should this be in User?
-    self.where(user_id: id).inject([]) { |memo, entry| memo.push entry.created_at.to_date; memo }.uniq[1..-1] || []
+  def self.archives_before_today(current_user)
+    self.archives_for_user(current_user.id)
   end
 
   def tag
@@ -28,5 +28,18 @@ class Entry < ActiveRecord::Base
     self.entry.split(' ').select do |t|
       t.exclude?('#')
     end.join(' ')
+  end
+
+private
+
+  def self.archives_for_user(id) # _dw should this be in User?
+    @archives_with_today = self.where(user_id: id).inject([]) { |memo, entry| memo.push entry.created_at.to_date; memo }
+    self.remove_today_from_archvies
+  end
+
+  def self.remove_today_from_archvies
+    @archives_with_today.select do |a|
+      a.to_date < Date.today
+    end.uniq || []
   end
 end
--- test/models/entry_test.rb
+++ test/models/entry_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class EntryTest < ActiveSupport::TestCase
+  setup do
+    @current_user = users(:one)
+  end
+
   test 'find the tags' do
     @entry = Entry.new(entry: 'this is a #tagged')
     assert_equal '#tagged', @entry.tag
@@ -9,5 +13,10 @@ class EntryTest < ActiveSupport::TestCase
   test 'find the title' do
     @entry = Entry.new(entry: 'this is a #tagged')
     assert_equal 'this is a', @entry.title
+  end
+
+  test 'lists archives_for_user' do
+    archives = Entry.archives_before_today(@current_user)
+    assert_equal 2, archives.count
   end
 end
