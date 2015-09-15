a3f79817679bd26b15f34959b8a1f5034557bd39
Dave Woodall <dave.woodall@livingsocial.com>
2015-09-15 07:42:06 -0600

    Only use devise with controller tests

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,10 +1,14 @@
 ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
-  # Setup all fixtures in test/fixtures/*.yml for all tests in alphabetical order.
   fixtures :all
+  # _dw needed for controller tests. Where do I put it?
+end
 
-  # Add more helper methods to be used by all tests here...
+class ActionController::TestCase
+  include Devise::TestHelpers
 end
