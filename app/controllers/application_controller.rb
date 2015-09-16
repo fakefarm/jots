8a7af79ec0d521aad83c66c5775f5372b7e8dc2e
Dave Woodall <dave.woodall@livingsocial.com>
2015-09-15 18:26:47 -0600

    Get rid of Application_presenter. That was a bad idea

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,26 +3,19 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!
   layout :layout_by_resource
   before_action :create_jot
-  before_action :stats
-  before_action :side_bar_nav
+  before_action :side_bar
 
   def create_jot
     @jot = Jot.new
   end
 
-  def side_bar_nav
+  def side_bar
     if current_user
-      @app_presenter = ApplicationPresenter.new(current_user)
-      @tag_list = Tag.where(user_id: current_user.id)
-    end
-  end
-
-  def stats
-    if current_user
-      @jot_count = Jot.where(user_id: current_user.id).count
-      @days = Jot.where(user_id: current_user.id).
-                        inject([]) { |memo, jot| memo.push jot.created_at.to_date; memo }.uniq.count
-      @tags = Tag.where(user_id: current_user.id).count
+      @jot_count = Jot.count(current_user)
+      @days = Jot.days(current_user)
+      @tags = Tag.count(current_user)
+      @tag_list = Tag.list_for_user(current_user)
+      @archives = Jot.archives_before_today(current_user)
     end
   end
 
