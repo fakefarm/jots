36b820f5253aaa9cde03187de76d908a6a44e953
Dave Woodall <dave.woodall@livingsocial.com>
2015-09-15 18:26:18 -0600

    Add #count to Tag

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -2,7 +2,11 @@ class Tag < ActiveRecord::Base
   has_many :jots
   validates :title, :user_id, presence: true
 
-  def self.list_for_user(id)
-    Tag.where(user_id: id)
+  def self.count(current_user)
+    Tag.where(user_id: current_user.id).count
+  end
+
+  def self.list_for_user(current_user)
+    Tag.where(user_id: current_user.id)
   end
 end
