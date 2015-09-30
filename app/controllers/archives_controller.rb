ae951a062c7d99ba665d0bf296b4aa89202a5e24
Dave Woodall <dave.woodall@livingsocial.com>
2015-09-30 17:11:07 -0600

    Show all archives at once

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,6 +1,6 @@
 class ArchivesController < ApplicationController
   def index
-    @archives = jots_for_day
+    @archives = jots_by_day
   end
 
   def show
@@ -13,7 +13,10 @@ private
     Jot.archives_before_today(current_user.id)
   end
 
-  def jots_for_day
-    @jots = current_user.jots.where("DATE(created_at) <> ?", Date.today).reverse
+  def jots_by_day
+    current_user.jots.where("created_at < ?", Date.today.beginning_of_day ). # gets all jots from user before today
+      order("created_at DESC"). # newest first
+      group_by {|j| j.created_at.to_date.strftime("%B, %d, %Y") } # rails method #group_by creates a hash and can use 2 arguments (key and value)
   end
 end
+
