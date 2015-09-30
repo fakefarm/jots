3fe7b2a2ab64d4bbacd847d233e38514fcb47fe3
Dave Woodall <dave.woodall@livingsocial.com>
2015-09-30 14:43:33 -0600

    Let’s use presenters later

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -1,9 +1,19 @@
 class ArchivesController < ApplicationController
   def index
-    @presenter = ArchivePresenter.new(current_user).jots
+    @archives = jots_for_day
   end
 
   def show
     @presenter = ArchivePresenter.new(current_user, date = params[:id]).jots_for_day
+  end
+
+private
+
+  def jots
+    Jot.archives_before_today(current_user.id)
+  end
+
+  def jots_for_day
+    @jots = current_user.jots.where("DATE(created_at) <> ?", Date.today).reverse
   end
 end
