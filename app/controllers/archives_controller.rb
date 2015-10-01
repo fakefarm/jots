1da13af0651c93c4787908a3bb14669796b1648c
Dave Woodall <dave.woodall@livingsocial.com>
2015-10-01 16:38:30 -0600

    Remove typo

--- app/controllers/archives_controller.rb
+++ app/controllers/archives_controller.rb
@@ -16,7 +16,7 @@ private
   def jots_by_day
     current_user.jots.where("created_at < ?", Date.today.beginning_of_day ). # gets all jots from user before today
       order("created_at DESC"). # newest first
-      group_by {|j| j.created_at.to_date.strftime("%B, %d, %Y") } # rails method #group_by creates a hash and can use 2 arguments (key and value)
+      group_by {|j| j.created_at.to_date.strftime("%B %d, %Y") } # rails method #group_by creates a hash and can use 2 arguments (key and value)
   end
 end
 
